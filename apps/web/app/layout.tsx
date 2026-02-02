061770fcb6df628eba17babd852899238172201e
RenatoMazzarino <renatomazzarinocorp@gmail.com>
2026-02-02 03:20:04 -0300

    fix(layout): ajustar peso da fonte Lato para incluir 300

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,7 +4,7 @@ import { Lato, Playfair_Display } from "next/font/google";
 
 const lato = Lato({
   subsets: ["latin"],
-  weight: ["400", "500", "700", "900"],
+  weight: ["300", "400", "700", "900"],
   variable: "--font-lato",
 });
 
